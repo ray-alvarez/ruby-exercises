5b7bc51b21b80aa6e23333842830413616e320b4
Ray Alvarez <niccolo1469@tutanota.com>
2019-07-18 20:05:46 -0700

    Add files via upload

---
+++ letters.rb
@@ -0,0 +1,42 @@
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+
+puts ''
+puts 'I like' + 'apple pie.'
+puts 'I like' + 'apple pie.'
+
+puts 'blink ' * 4
+puts ''
+puts 'moo ' * 3
+
+# Adding an apostrophe make a difference 
+# between number or string.
+puts ''
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts ''
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+=begin
+puts ''
+puts '12' + 12
+puts '2' * '5'
+
+
+puts 'Betty' + 12
+puts 'Fred' * 'John'
+=end
+
+# Place a backsplash for escape character
+puts ''
+puts 'You\'re swell!'
+
+puts ''
+puts 'backsplash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\\down'
